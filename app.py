97f3319660b56b2eed3244ef2686c7e9a0502a0f
Pijush Chakraborty <234633224+pijushrkmvm-spec@users.noreply.github.com>
2025-12-25 22:59:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # 3. Header
 st.title("🔬 Germiverse Bio-Suite")
-st.markdown("### Advanced Computational Tools for Biologists")
+st.markdown("### Computational Tools for Biologists")
+st.markdown("### Best viewed in Desktop, please turn on Desktop Mode if you are using Mobile")
 st.markdown("---")
 
 # 4. Introduction
